2ef8efa2846b050f4221d28ac7a3ade6b8bbd7d8
Snuzzo <jrracinfan@gmail.com>
2014-03-27 16:37:03 -0400

    Telephony & Boot Ani bring-up

--- ill.mk
+++ ill.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common ILL stuff.
-$(call inherit-product, vendor/illusion/config/common.mk)
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d800/d800.mk)
@@ -20,8 +20,6 @@ PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_att_us/g2:4.2.2/JDQ39B/D80010d.1376460177:user/release-keys PRIVATE_BUILD_DESC="g2_att_us-user 4.2.2 JDQ39B D80010d.1376460177 release-keys"
 
-PRODUCT_PACKAGES += Torch
-
 # bootanimation
 PRODUCT_COPY_FILES += \
-        vendor/illusion/bootanimation/Illusion768x768.zip:system/media/bootanimation.zip
+        vendor/illusion/bootanimation/Illusion1080x1080.zip:system/media/bootanimation.zip
